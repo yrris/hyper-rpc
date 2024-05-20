f2180d3b3834e0da9746bd9b325f7013587adbf4
yrris <yrriswift7@gmail.com>
2024-05-20 22:11:37 +0800

    通过SPI机制支持自定义序列化器

--- hyper-rpc-core/src/main/java/com/yrris/hyperrpc/serializer/SerializerFactory.java
+++ hyper-rpc-core/src/main/java/com/yrris/hyperrpc/serializer/SerializerFactory.java
@@ -1,5 +1,7 @@
 package com.yrris.hyperrpc.serializer;
 
+import com.yrris.hyperrpc.spi.SpiLoader;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,28 +10,23 @@ import java.util.Map;
  * 用于获取序列化器对象
  */
 public class SerializerFactory {
-    /**
-     * 序列化映射，用于实现单例
-     */
-    private static final Map<String,Serializer> KEY_SERIALIZER_MAP = new HashMap<String,Serializer>(){{
-        put(SerializerKeys.JDK,new JdkSerializer());
-        put(SerializerKeys.KRYO,new KryoSerializer());
-        put(SerializerKeys.JSON,new JsonSerializer());
-        put(SerializerKeys.HESSIAN,new HessianSerializer());
-    }};
+
+    static {
+        SpiLoader.load(Serializer.class);
+    }
 
     /**
      * 默认序列化器为jdk
      */
-    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get(SerializerKeys.JDK);
+    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
 
     /**
      * 获取序列化器实例
      * @param key
      * @return
      */
-    public static Serializer getInstance(String key){
-        return KEY_SERIALIZER_MAP.getOrDefault(key,DEFAULT_SERIALIZER);
+    public static Serializer getInstance(String key) {
+        return SpiLoader.getInstance(Serializer.class, key);
     }
 
 
